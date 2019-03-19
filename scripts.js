1cd68e7e49cb6d8d0fc8a1713d7fe86db37b9553
MarinaBaidyk <marina.baidyk@catenamedia.com>
2019-03-19 17:14:27 +0200

    added my js for sticky

---
+++ scripts.js
@@ -0,0 +1,41 @@
+var elem = document.getElementById('sticky-block');
+var elemWrap = document.getElementById('sticky-wrapper');
+var elemTop = elem.getBoundingClientRect().top + window.pageYOffset;
+var elemHeight = elem.offsetHeight;
+var fooElem = document.querySelector('footer');
+var fooTop = fooElem.offsetTop;
+var indentTop = elem.offsetTop;
+
+window.onscroll = myScroll;
+window.onresize = resize;
+
+function setWidth() {
+    var padding = (parseInt(getComputedStyle(elemWrap).paddingLeft) * 2).toFixed();
+    var width = elemWrap.offsetWidth - padding;
+    elem.style.width = width + 'px';
+}
+
+function myScroll() {
+    setWidth();
+    if (window.pageYOffset + indentTop > elemTop) {
+        elem.classList.add('fixed');
+    } else {
+        elem.classList.remove('fixed');
+    } 
+
+    if (window.pageYOffset + elemHeight + (2 * indentTop) > fooTop) {
+        elem.style.top = -(window.pageYOffset + (elemHeight + indentTop - fooTop)) + 'px';
+    } else {
+        elem.style.top = indentTop + "px";
+    }    
+};
+
+function resize() {
+ if (window.matchMedia('(min-width: 1200px)').matches) {
+        window.onscroll = myScroll;
+        setWidth();
+    } else {
+        elem.style.width = 'auto';
+    }
+}
+
